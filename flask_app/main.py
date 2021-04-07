903c1be3d72d66d82ac39b9d030e36bee4cf9354
Rakhid16 <roploverz@gmail.com>
2021-04-07 16:09:28 +0700

    main flask app

---
+++ flask_app/main.py
@@ -0,0 +1,19 @@
+from joblib import load
+from flask import Flask, render_template
+
+app = Flask(__name__)
+model = load("../modeling/simple_model.pkl")
+
+@app.route("/")
+def index():
+  return {"data" : "hello world"}
+
+@app.route("/html_css_js/<sentence>")
+def html_css_js(sentence):
+  return render_template("index.html", data = sentence)
+
+@app.route("/ml_serving/<sentence>")
+def ml_serving(sentence):
+  return model.classify(sentence)
+
+app.run(debug = True)
